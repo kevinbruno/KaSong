af6157b6f593bb27aafcc5afb149c5036ebe1723
kevinbruno <kevinbrunotablet@gmail.com>
2023-02-17 18:14:49 -0300

    .

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,7 @@ const capaMusica = document.getElementById('imagen')
 const nomeMusica = document.getElementById('titulo')
 const bandaNome = document.getElementById('nome-artista')
 const tempoMusica = document.getElementById('tempo-musica')
+const barraCiclavel = document.getElementById("constainer-progresso")
 const barraProgresso = document.getElementById('barra-tempo')
 const progressoMusica = document.getElementById('progresso-atual')
  // tag de audio
@@ -12,6 +13,11 @@ const aleatorio = document.getElementById('embaralhar')
 const anterior = document.getElementById('anterior')
 const play = document.getElementById("tocar")
 const proxima = document.getElementById('proxima')
+const repetir = document.getElementById('repetir')
+const tempoAtual = document.getElementById('tempo-atual')
+const tempoTotal = document.getElementById('tempo-total')
+
+musica.pause
 
  // constantes com as infos das musicas
 const asItWas = {
@@ -78,24 +84,53 @@ const money = {
     'tempo': '3:54'
 }
  // playlist (com todas as variaveis com as informações das) e indice
-const playlist = [asItWas, beggin, bones, m505, chlorine, heathens, 
+const playlistOriginal = [asItWas, beggin, bones, m505, chlorine, heathens, 
     spaceSong, young, money]
+// os '...' espalha os dados do array para este nv array
+let playlistEmbaralhada = [...playlistOriginal] 
+
+
 let index = 0 // indice da música
 musica.volume = .6
 let tocando = false
 let porcentagem // para a barra de tempo
+let repetição = false
+let embaralhado = false
 
 function configurarMusica() {
-    capaMusica.src = playlist[index].capa
-    nomeMusica.innerText = playlist[index].nome
-    bandaNome.innerText = playlist[index].artista
-    musica.src = playlist[index].musica
-    //tempoMusica.innerText = playlist[index].tempo
-    musica.ontimeupdate = barraTempo
-    barraProgresso.onclick = definirTempo
+    capaMusica.src = playlistEmbaralhada[index].capa
+    nomeMusica.innerText = playlistEmbaralhada[index].nome
+    bandaNome.innerText = playlistEmbaralhada[index].artista
+    musica.src = playlistEmbaralhada[index].musica
+    //tempoMusica.innerText = playlistEmbaralhada[index].tempo
+    /*musica.ontimeupdate = barraTempo
+    barraProgresso.onclick = definirTempo*/
 }
 
 configurarMusica()
+
+function formatarTempo(númeroOriginal) {
+    let horas = Math.floor(númeroOriginal/3600)
+    console.log(horas)
+    let mins = Math.floor((númeroOriginal-horas*3600) / 60)
+    console.log(mins)
+    let segs = Math.floor((númeroOriginal-horas*360-mins*60))
+    console.log(segs)
+    horas = horas.toString().padStart(2, '0')
+    mins = mins.toString().padStart(2, '0')
+    segs = segs.toString().padStart(2, '0')
+    if (horas != '00') {
+        return `${horas}:${mins}:${segs}`
+    } else {
+        return `${mins}:${segs}`
+    }
+}
+function atualizarTempoAtual() {
+    tempoAtual.innerText = formatarTempo(musica.currentTime)
+}
+function atualizarTempoTotal() {
+    tempoTotal.innerText = formatarTempo(musica.duration)
+}
 
 function tocarMusica(){
      // classe Bootstrap Icon para mudar icone
@@ -107,20 +142,81 @@ function pausarMusica(){
     play.querySelector('.bi').classList.replace('bi-pause-circle-fill', 'bi-play-circle-fill')
     musica.pause()
 }
+function atualizarProgresso() {
+    const larguraBarra = (musica.currentTime/musica.duration)*100
+    progressoMusica.style.setProperty('--progresso', `${larguraBarra}%`)
+    atualizarTempoAtual()
+}
+function pularPara(evento) {
+    const largura = barraProgresso.offsetWidth // largura da barra
+    /* evento.offsetX == largura em px aonde foi clicado*/
+    const clickPos = evento.offsetX
+    const pularParaTempo = (clickPos/largura)*musica.duration
+    musica.currentTime = pularParaTempo
+}
+
+ /* meu sem o teacher
+function acabou() {
+    if (repetição) {
+        musica.currentTime = 0
+        musica.play()
+    } else {
+        proximaFunc()
+    }
+}*/
+function embaralhar(preEmbarArray) {
+    const tamanho = preEmbarArray.length // pega o tamanho do array
+    let indexEM = tamanho - 1
+    /*
+    NÃO há nescessidade de embaralhar o primeiro elemento, pois
+    ele começa da direita para esquerda(do 'final') logo
+    quando chegar no primeiro elemento ele ja acabou sendo
+    embaralhado, pois se todos os outros ja foram, alguma
+    hora alguem trocou com ele!
+    */
+    while (indexEM > 0) {
+        let indexAleatorio = Math.floor(Math.random() * tamanho)
+        let aux = preEmbarArray[indexEM]
+        /* função de trocar os valores (o escolhido com o ultimo) */
+        preEmbarArray[indexEM] = preEmbarArray[indexAleatorio]
+        preEmbarArray[indexAleatorio] = aux
+        /*  */
+        indexEM--
+    }
+    console.log(playlistEmbaralhada)
+}
+function acabou() {
+    if (repetição) {
+        tocarMusica()
+    } else {
+        proximaFunc()
+    }
+}
+/*
 function barraTempo() {
     porcentagem = musica.currentTime*100 / musica.duration
     porcentagem = porcentagem.toFixed(2) + '%'
     progressoMusica.style.width = porcentagem
     if (musica.currentTime == musica.duration) {
-        proximaFunc()
+        acabou()
     }
 }
 function definirTempo(e) {
     musica.currentTime = (e.offsetX/barraProgresso.offsetWidth) * musica.duration
 }
-
+*/
+/* fiz por conta kk
+function repetirFunc() {
+    repetição = !repetição
+    if (repetição) {
+        repetir.querySelector('.bi').classList.replace('bi-repeat', 'bi-repeat-1')
+    } else {
+        repetir.querySelector('.bi').classList.replace('bi-repeat-1', 'bi-repeat')
+    }
+}*/
+/* do video fora do curso, aqui é aleatorio e n embaralhar
 function aleatorioFunc() {
-    let numero = Math.floor(Math.random() * playlist.length)
+    let numero = Math.floor(Math.random() * playlistEmbaralhada.length)
     if (numero != index) {
         index = numero
     } else {
@@ -128,12 +224,23 @@ function aleatorioFunc() {
     }
     configurarMusica()
     tocarMusica()
+}*/
+function embaralhadoClick(){
+    if (embaralhado) {
+        embaralhado = false
+        playlistEmbaralhada = [...playlistOriginal]
+        aleatorio.classList.remove('botao-ativo')
+    } else {
+        embaralhado = true
+        embaralhar(playlistEmbaralhada)
+        aleatorio.classList.add('botao-ativo')
+    }
 }
 function anteriorFunc() {
     if (index > 0) {
         index --
     }else if (index == 0){
-        index = playlist.length - 1
+        index = playlistEmbaralhada.length - 1
     }
     configurarMusica()
     tocarMusica()
@@ -147,17 +254,32 @@ function botaoPlay() {
     tocando = !tocando
 }
 function proximaFunc() {
-    if (index < (playlist.length - 1)) {
+    if (index < (playlistEmbaralhada.length - 1)) {
         index ++
-    }else if (index == (playlist.length -1)){
+    }else if (index == (playlistEmbaralhada.length -1)){
         index = 0
     }
     configurarMusica()
     tocarMusica()
 }
+function repetirFunc() {
+    if (repetição) {
+        repetir.classList.remove('botao-ativo')
+    } else {
+        repetir.classList.add('botao-ativo')
+    }
+    repetição = !repetição
+}
 
  // monitora e responde os eventos dos botões
-aleatorio.addEventListener('click', aleatorioFunc)
+aleatorio.addEventListener('click', embaralhadoClick)
 anterior.addEventListener('click', anteriorFunc)
 play.addEventListener('click', botaoPlay)
 proxima.addEventListener('click', proximaFunc)
+repetir.addEventListener('click', repetirFunc)
+
+//song
+barraCiclavel.addEventListener('click', pularPara)
+musica.addEventListener('timeupdate', atualizarProgresso)
+musica.addEventListener('ended', acabou)
+musica.addEventListener('loadedmetadata', atualizarTempoTotal)
